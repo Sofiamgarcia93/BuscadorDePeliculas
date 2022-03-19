6d59f267260d84ef7c0884911a002a3090037b45
Sofia <sofiamgarcia93@hotmail.com>
2022-03-19 17:49:04 -0300

    agrego grid y box a home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,48 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Carrusel from "./Carrusel";
 import Listados from "./Listados";
 import "./Home.css";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid'
 
 const Home = () =>{
     return(
-        <div className="Home">
-            <h2>Soy el Home</h2>
+       <div> 
+        <Grid container spacing ={4} direction="row"
+        justifyContent="center"
+        alignItems="center"
+        >
+
+        <Grid item xs={12} sx={{borderColor: 'error.main'}}>
+        <Box >
+        <h2>Soy el Home</h2>
             <Carrusel />
+        </Box>
+        </Grid>
+
+        <Grid item xs={6}>
+        <Box>
+
             <Listados 
             titulo="Peliculas Populares"
             url="popular"
             />
+        </Box>
+        </Grid>
+
+        <Grid item xs={6}>
+        <Box>
             <Listados
             titulo="Peliculas Mejor Puntuadas"
             url="top_rated"
             />
-        </div>
+        </Box>
+        </Grid>
+
+
+            
         
+      </Grid>
+      </div>
     )
 }
 
