9a4def24a706213d6e3fd739375acfd9f6e6fc8d
Sofia <sofiamgarcia93@hotmail.com>
2022-03-19 18:17:00 -0300

    hago hook en lanzamientos y en populares

--- src/components/Populares.js
+++ src/components/Populares.js
@@ -1,11 +1,14 @@
 import Tarjeta from "./Tarjeta"
-
+import useFetchPeliculas from "../hooks/useFetchPeliculas"
 
 const Populares = () =>{
-
+    const peliculas = useFetchPeliculas("popular");
     return(
         <div>
             <h2>Soy componente Populares</h2>
+            {peliculas.map(pelicula => (
+        <h3 key={pelicula.id}>{pelicula.title}</h3>
+      ))}
             <Tarjeta />
         </div>
     )
