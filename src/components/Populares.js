ce4fe73d79e03278f0fa004c915389d30f4bbb81
Sofia <sofiamgarcia93@hotmail.com>
2022-03-23 22:00:54 -0300

    agrego componente tarjeta  popular y ultimos lanzamientos

--- src/components/Populares.js
+++ src/components/Populares.js
@@ -1,16 +1,50 @@
 import Tarjeta from "./Tarjeta"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
 import useFetchPeliculas from "../hooks/useFetchPeliculas"
 
 const Populares = () =>{
     const peliculas = useFetchPeliculas("popular");
+    console.log(peliculas[0]?.title)
     return(
-        <div>
-            <h2>Soy componente Populares</h2>
-            {peliculas.map(pelicula => (
-        <h3 key={pelicula.id}>{pelicula.title}</h3>
+     
+        <Box
+        sx={{
+          marginTop: 10,
+          height: 80,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 1,
+        }}
+      >
+        <Typography
+          sx={{
+            bgcolor: "#6fbf73",
+            color: "white",
+          }}
+          variant="h5"
+          gutterBottom
+          component="div"
+        >
+          Populares
+        </Typography>
+
+      <Box sx={{
+          display: "flex",
+          flexWrap: "wrap",
+        
+        }}>
+      {peliculas.map((pelicula) => (
+         <Tarjeta
+         key={pelicula.id}
+         titulo={pelicula.title}
+         urlImg={`https://image.tmdb.org/t/p/w300/${pelicula.poster_path}`}
+         linkTarjeta={`/detalle-pelicula/${pelicula.id}`}
+       />
       ))}
-            <Tarjeta />
-        </div>
+      </Box>
+    </Box>
+      
     )
 }
 
--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -1,12 +1,55 @@
-import Box from '@mui/material/Box';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import { Link } from "react-router-dom";
+import CardActions from "@mui/material/CardActions";
+import { IconButton } from "@mui/material";
 
-const Tarjeta = () =>{
-    return(
-        <Box>
-            
-        </Box>
-    )
-}
-
+const Tarjeta = ( {titulo, urlImg, linkTarjeta}) => {
+  return (
+    <Card
+      sx={{
+        width: 250,
+        heigth: 370,
+        m: 2,
+        display: "flex",
+        flexDirection: "column;",
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="140"
+        image={urlImg}
+        alt="imagen de pelicula"
+      />
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="h6"
+          component="div"
+          sx={{
+            width: 350,
+            whiteSpace: "nowrap",
+            textOverFlow: "ellipsis",
+            overflow: "hidden",
+            textAling: "center",
+          }}
+        >
+          {titulo}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ justifyContent: "center" }}>
+        <Link to={linkTarjeta}>
+          <IconButton style={{ color: "#1565c0" }}>
+            <RemoveRedEyeIcon></RemoveRedEyeIcon>
+          </IconButton>
+        </Link>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default Tarjeta;
