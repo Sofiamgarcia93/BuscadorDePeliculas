47dbbeac98289d6dbcf2f7f5397033b2f938f17c
Sofia <sofiamgarcia93@hotmail.com>
2022-03-29 16:24:03 -0300

    genero footer y agrego links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,54 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import IconButton from "@mui/material/IconButton";
+
 const Footer = () =>{
     return(
-        <div>
-            <h3>Soy el Footer</h3>
-        </div>
+        <Box
+        sx={{
+          flexGrow: 1,
+          flexDirection: "column",
+          textAlign: "center",
+          bgcolor: "#424242",
+          height: 80,
+        }}
+      >
+        <Typography
+          sx={{
+            color: "white",
+          }}
+          variant="h6"
+          component="div"
+        >
+          Hecho por Sofia Garcia
+        </Typography>
+   <IconButton
+          target="_black"
+          href="https://github.com/Sofiamgarcia93/BuscadorDePeliculas"
+          sx={{ color: "white"}}
+        >
+          <GitHubIcon fontSize='medium' ></GitHubIcon>
+        </IconButton>
+  
+        <IconButton
+          target="_black"
+          href="https://www.linkedin.com/in/sofia-macarena-garcia-649bb7203/"
+          sx={{ color: "white" }}
+        >
+          <LinkedInIcon fontSize='medium' ></LinkedInIcon>
+        </IconButton>
+  
+        <IconButton
+          target="_black"
+          href="sofiamgarcia98@gmail.com"
+          sx={{ color: "white" }}
+        >
+          <EmailIcon  fontSize='medium'></EmailIcon>
+        </IconButton>
+      </Box>
     
     )
 }
