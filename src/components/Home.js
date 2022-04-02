4c02b45e15f2bd337c85ccad9170d365980bffc0
Sofia <sofiamgarcia93@hotmail.com>
2022-04-02 17:58:17 -0300

    agrego flexwrap en listados

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,47 +3,36 @@ import Carrusel from "./Carrusel";
 import Listados from "./Listados";
 import "./Home.css";
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid'
+
 
 const Home = () =>{
     return(
-       <div> 
-        <Grid container spacing ={4} direction="row"
-        justifyContent="center"
-        alignItems="center"
-        >
-
-        <Grid item xs={12} sx={{borderColor: 'error.main'}}>
-        <Box >
-       
+       <Box> 
             <Carrusel />
-        </Box>
-        </Grid>
-
-        <Grid item xs={4}>
-        <Box>
-
+        <Box
+        sx={{
+            display: "flex",
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+            p: 5,
+        }}>
             <Listados 
             titulo="Peliculas Populares"
             url="popular"
             />
-        </Box>
-        </Grid>
 
-        <Grid item xs={4}>
-        <Box>
             <Listados
             titulo="Peliculas Mejor Puntuadas"
             url="top_rated"
             />
         </Box>
-        </Grid>
+      
 
 
             
         
-      </Grid>
-      </div>
+    
+      </Box>
     )
 }
 
